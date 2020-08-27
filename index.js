311ea2f03c95868807b22e1914879bcfe0365cc1
ejw773 <ejw773@gmail.com>
2020-08-27 06:31:12 -0400

    adds watchlist page

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-$(document).ready(function() {
+$(function(){
     function renderMovies(movieArray) {
         movieArray.map((currentMovie) => {
-            $(".movie-container").append(`<div class="movie"><div class="card" style="width: 18rem;"><img class="card-img-top" src="${currentMovie.Poster}" alt="${currentMovie.Title}"><div class="card-body"><h5 class="card-title">${currentMovie.Title}</h5><p class="card-text">${currentMovie.Year}</p><a href="#" class="btn btn-primary">Add</a></div></div></div>`)
+            $(".movie-container").append(`<div class="movie"><div class="card" style="width: 18rem;"><img class="card-img-top" src="${currentMovie.Poster}" alt="${currentMovie.Title}"><div class="card-body"><h5 class="card-title">${currentMovie.Title}</h5><p class="card-text">${currentMovie.Year}</p><a href="#" class="btn btn-primary" onclick="saveToWatchlist('${currentMovie.imdbID}')">Add</a></div></div></div>`)
         })
     }
-    renderMovies(movieData);
+    $('form').submit(function(e){
+        e.preventDefault();
+        $('.movie-container').empty();
+        renderMovies(movieData);
+    })
+    
 });
+
+function saveToWatchlist(imdbID) {
+    var movie = movieData.find(function(currentMovie) {
+        return currentMovie.imdbID == imdbID;
+    var watchlistJSON = localStorage.getItem('watchlist');
+    var watchlist = JSON.parse(watchlistJSON);
+    if (watchlist === null) {
+        watchlist = [];
+    } else {
+        var watchlist = JSON.parse(watchlistJSON);
+    };
+    console.log(imdbID);
+    watchlist.push(movie);
+    watchlistJSON = JSON.stringify(watchlist);
+    localStorage.setItem('watchlist', watchlistJSON);
+    });
+}
